f1936af681f5c9d7b7ccaa1a5e57bdb4c44e8207
abhishekmsipl <abhishek.maheshwari@systematixindia.com>
2021-02-19 12:34:34 +0530

    first commit

---
+++ app/src/main/java/com/dichter/neira/sample/sampleapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dichter.neira.sample.sampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
